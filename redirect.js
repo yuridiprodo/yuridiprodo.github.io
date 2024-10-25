bef29caf9d0293f838b6a8a23fad365a04be1505
ronw4 <yuri87diprodo@gmail.com>
2024-10-25 14:46:10 +0200

    reinderizzamento semiforzato

--- redirect.js
+++ redirect.js
@@ -1,4 +1,7 @@
 console.log("Redirect script started");
 
-// Forza il caricamento della index
-window.location.href = 'index.html';
+// Controlla se non siamo già sulla pagina index.html
+if (window.location.pathname !== '/index.html') {
+    // Forza il caricamento della index
+    window.location.href = 'index.html';
+}
