9f53d8cc76d28116f2f72271d1b9654d181c1a1e
ronw4 <yuri87diprodo@gmail.com>
2025-01-17 15:49:31 +0100

    rimosso breadcrumb

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 const articlesDiv = document.getElementById('articles');
 
-// Funzione per generare il breadcrumb dinamicamente
-function updateBreadcrumb(path) {
-    const breadcrumbDiv = document.getElementById('breadcrumb');
-    const segments = path.split('/').filter(Boolean); // Dividi il percorso in segmenti e rimuovi quelli vuoti
-
-    let breadcrumbHTML = '<a href="/index.html">Home</a>'; // Sempre un link alla home come primo elemento
-    let currentPath = '';
-
-    // Aggiungi gli altri segmenti del breadcrumb
-    segments.forEach((segment, index) => {
-        currentPath += `/${segment}`;
-        breadcrumbHTML += ` &gt; <a href="${currentPath}">${decodeURIComponent(segment)}</a>`;
-    });
-
-    breadcrumbDiv.innerHTML = breadcrumbHTML;
-    breadcrumbDiv.style.display = 'block'; // Mostra il breadcrumb
-}
-
 // Funzione per caricare la home
 async function loadHome() {
     try {
@@ -31,9 +13,6 @@ async function loadHome() {
         
         // Nascondi il footer menu per la home
         document.getElementById('footer-menu').style.display = 'none';
-		
-		// Nascondi il breadcrumb
-        document.getElementById('breadcrumb').style.display = 'none';
 		
         // Aggiorna l'URL nella barra degli indirizzi
         window.history.pushState(null, '', '/');
@@ -53,9 +32,6 @@ async function loadArticle(articleName) {
         if (!response.ok) throw new Error('File non trovato');
         const markdown = await response.text();
         const html = marked(markdown);
-		
-        // Mostra il breadcrumb
-        updateBreadcrumb(`/articles/${articleName}`);
 		
 	    // Mostra il footer menu
 	    document.getElementById('footer-menu').style.display = 'block';
@@ -83,9 +59,6 @@ async function loadPages(pageName) {
         if (!response.ok) throw new Error('File non trovato');
         const markdown = await response.text();
         const html = marked(markdown);
-		
-        // Mostra il breadcrumb
-        updateBreadcrumb(`/pages/${pageName}`);
 		
 	    // Mostra il footer menu
 	    document.getElementById('footer-menu').style.display = 'block';
@@ -179,18 +152,14 @@ window.onpopstate = (event) => {
     }
 };
 
-// Modifica la funzione che gestisce il caricamento iniziale
+// Gestione del caricamento iniziale
 window.onload = () => {
     const path = window.location.pathname;
-    const matchArticle = path.match(/articles\/(.+)\.html/);
-    const matchPage = path.match(/pages\/(.+)\.html/);
-
-    if (matchArticle) {
-        loadArticle(matchArticle[1]);
-    } else if (matchPage) {
-        loadPages(matchPage[1]);
+    const match = path.match(/articles\/(.+)\.html/);
+    if (match) {
+        loadArticle(match[1]);
     } else {
-        loadHome(); // Carica la home se nessuna corrispondenza
+        loadHome();
     }
 };
 
